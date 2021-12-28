193bb9d1c026b99cc975e9b087434739bc33c4d1
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2021-12-28 14:16:50 +0200

    Revert "Base + templet"
    This reverts commit ca617acd507bb2bc6a68e0d0e077efdea24ad02a.

--- storage/framework/views/b870129165ea71afe2843b33c4ccb3f76e49a664.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title'); ?>
-    Abo nasser
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('content'); ?>
-    <h1>loreem</h1>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('adminLayouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\dashboard\Lara8\resources\views/home.blade.php ENDPATH**/ ?>
