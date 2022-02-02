4786aad57c5bfa601d1b20e3d72d08eb936bab83
Eslam-atia442 <56205474+Eslam-atia442@users.noreply.github.com>
2022-02-02 13:06:37 +0200

    Update index.blade.php

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -211,7 +211,7 @@
                             <div class="team-img">
                                 <!-- team img start -->
                                 <a href="#" class="main-img">
-                                    <img src="{{$partner->images}}" style="" alt="">
+                                    <img src="{{$partner->images}}" style="" alt=""> <br><br><br>
                                 </a>
                                 <!-- team img end -->
                                 <!-- team info start -->
