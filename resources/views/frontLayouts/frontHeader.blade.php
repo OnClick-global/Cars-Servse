05669f544f5b22ae7741eb780ef6306fb99bb710
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-02-08 14:19:51 +0200

    Update frontHeader.blade.php

--- resources/views/frontLayouts/frontHeader.blade.php
+++ resources/views/frontLayouts/frontHeader.blade.php
@@ -66,7 +66,7 @@
     <!-- style css
     ============================================ -->
     @if(app()->getLocale() == 'ar')
-        <link rel="stylesheet" href="{{url('/')}}/assets/front/style.rtl.css?v=1.0">
+        <link rel="stylesheet" href="{{url('/')}}/assets/front/style.rtl.css?v=1.1">
     @else
         <link rel="stylesheet" href="{{url('/')}}/assets/front/style.css?v=1">
     @endif
