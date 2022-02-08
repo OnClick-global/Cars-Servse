3db43bc3351ba3ad1b851716c386cd3ba033bce3
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-02-08 16:55:34 +0200

    csssss

--- resources/views/frontLayouts/frontHeader.blade.php
+++ resources/views/frontLayouts/frontHeader.blade.php
@@ -26,7 +26,12 @@
 
     <!-- bootstrap css
     ============================================ -->
-    <link rel="stylesheet" href="{{url('/')}}/assets/front/css/bootstrap.min.css">
+    @if(app()->getLocale() == 'ar')
+        <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css" integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
+    @else
+        <link rel="stylesheet" href="{{url('/')}}/assets/front/css/bootstrap.min.css">
+    @endif    
+
 
     <!-- meanmenu css
     ============================================ -->
@@ -66,7 +71,7 @@
     <!-- style css
     ============================================ -->
     @if(app()->getLocale() == 'ar')
-        <link rel="stylesheet" href="{{url('/')}}/assets/front/style.rtl.css?v=1.3">
+        <link rel="stylesheet" href="{{url('/')}}/assets/front/style.rtl.css?v=1.4">
     @else
         <link rel="stylesheet" href="{{url('/')}}/assets/front/style.css?v=1">
     @endif
