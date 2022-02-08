c073d675c977e437922b9c62c2740f3c1b6818ac
Khaled Elfishawy <khalidelfishawy@gmail.com>
2022-02-08 13:48:35 +0200

    Update frontSidebar.blade.php

--- resources/views/frontLayouts/frontSidebar.blade.php
+++ resources/views/frontLayouts/frontSidebar.blade.php
@@ -3,27 +3,30 @@
     <div class="container">
         <div class="menu-position">
             <div class="row">
-                <div class="col-md-12 col-sm-10 static">
+                <div class="col-md-3 col-sm-2">
+                    <!-- logo start -->
+                    <div class="logo">
+                        <a href="{{route('front')}}">
+                            <img style="height: 50px;" src="{{asset('uploads/setting')}}/{{\App\Models\Setting::where('key','logo')->first()->value ?? ''}}" alt="">
+                        </a>
+                    </div>
+                    <!-- logo end -->
+                </div>                
+                <div class="col-md-9 col-sm-10 static">
                     <!-- main-menu start -->
                     <div class="main-menu">
                         <nav>
                             <ul>
                                 <!-- single menu -->
-
-                                <!-- single menu -->
-                                <li>
-                                    <div class="logo">
-                                        <a href="{{route('front')}}">
-                                            <img style="height: 50px;" src="{{asset('uploads/setting')}}/{{\App\Models\Setting::where('key','logo')->first()->value ?? ''}}" alt="">
-                                        </a>
-                                    </div>
-                                </li>
                                 <li><a href="{{route('front')}}#aboutCS">{{App\Helpers\Helper::translate('about company')}}</a></li>
                                 <li><a href="{{route('front')}}#Partners">{{App\Helpers\Helper::translate('Partners')}}</a></li>
                                 <li><a href="{{route('front')}}#Clients">{{App\Helpers\Helper::translate('Clients')}}</a></li>
                                 <li><a href="{{route('front')}}#Services">{{App\Helpers\Helper::translate('Services')}}</a></li>
                                 <li><a href="{{route('tensoView')}}">{{App\Helpers\Helper::translate('tenso')}}</a></li>
-                                <li class="has-sub"><a href="javascript:void(this);">{{App\Helpers\Helper::translate('Cars')}}<i class="icofont icofont-simple-down"></i></a>
+                                <li class="has-sub">
+                                    <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Cars')}}
+                                        <i class="icofont icofont-simple-down"></i>
+                                    </a>
                                     <!-- sub menu start -->
                                     @php($cars=\App\Models\Car::where('status','1')->get())
                                     <ul class="sub-menu left">
