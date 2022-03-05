23e9655c867fd12d5a1b9a3ad4d987cd5d74e823
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-03-05 14:40:37 +0200

    fix navbar

--- resources/views/frontLayouts/frontSidebar.blade.php
+++ resources/views/frontLayouts/frontSidebar.blade.php
@@ -14,6 +14,12 @@
                     </div>
                     <!-- logo end -->
                 </div>
+                @php
+                    $cars=\App\Models\Car::where('status','1')->get();
+                    $isuzu_services=\App\Models\Service::where('type','isuzu')->get();
+                    $denso_services=\App\Models\Service::where('type','denso')->get();
+                    $services=\App\Models\Service::where('type','services')->get();
+                @endphp
                 <div class="col-md-10 col-sm-10 static">
                     <!-- main-menu start -->
                     <div class="main-menu">
@@ -24,8 +30,6 @@
                                     <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Cars')}}
                                         <i class="icofont icofont-simple-down"></i>
                                     </a>
-                                    <!-- sub menu start -->
-                                    @php($cars=\App\Models\Car::where('status','1')->get())
                                     <ul class="sub-menu left">
                                         @foreach($cars as $car)
                                             <li><a href="{{route('carFrontView',$car->id)}}">
@@ -40,25 +44,22 @@
                                         <i class="icofont icofont-simple-down"></i>
                                     </a>
                                     <!-- sub menu start -->
-                                    @php($isuzu_services=\App\Models\Service::where('type','isuzu')->get())
                                     <ul class="sub-menu left">
                                         @foreach($isuzu_services as $row)
-                                            <li><a href="{{route('tensoView')}}">
+                                            <li><a href="{{route('serviceFrontView',$row->id)}}">
                                                     {{ app()->getLocale() == 'en' ? $row->name_en : $row->name_ar }}
                                                 </a></li>
                                         @endforeach
                                     </ul>
                                     <!-- sub menu end -->
                                 </li>
-
                                 <li class="has-sub">
                                     <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Tenso')}}
                                         <i class="icofont icofont-simple-down"></i>
                                     </a>
                                     <!-- sub menu start -->
-                                    @php($services=\App\Models\Service::where('type','denso')->get())
                                     <ul class="sub-menu left">
-                                        @foreach($services as $row)
+                                        @foreach($denso_services as $row)
                                             <li><a href="{{route('serviceFrontView',$row->id)}}">
                                                     {{ app()->getLocale() == 'en' ? $row->name_en : $row->name_ar }}
                                                 </a></li>
@@ -71,7 +72,7 @@
                                         <i class="icofont icofont-simple-down"></i>
                                     </a>
                                     <!-- sub menu start -->
-                                    @php($services=\App\Models\Service::where('type','services')->get())
+
                                     <ul class="sub-menu left">
                                         @foreach($services as $row)
                                             <li><a href="{{route('serviceFrontView',$row->id)}}">
@@ -114,35 +115,76 @@
                 <div class="mobile-menu">
                     <nav>
                         <ul>
-
-                            <li>
-                                <a href="{{route('front')}}#aboutCS">{{App\Helpers\Helper::translate('about company')}}</a>
-                            </li>
-                            <li><a href="{{route('front')}}#Partners">{{App\Helpers\Helper::translate('Partners')}}</a>
-                            </li>
-                            <li><a href="{{route('front')}}#Clients">{{App\Helpers\Helper::translate('Clients')}}</a>
-                            </li>
-                            <li><a href="{{route('front')}}#Services">{{App\Helpers\Helper::translate('Services')}}</a>
-                            </li>
-                            <li><a href="{{route('tensoView')}}">{{App\Helpers\Helper::translate('tenso')}}</a></li>
-                            <li class="has-sub"><a
-                                    href="javascript:void(this);">{{App\Helpers\Helper::translate('Cars')}}<i
-                                        class="icofont icofont-simple-down"></i></a>
+                            <!-- single menu -->
+                            <li class="has-sub">
+                                <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Cars')}}
+                                    <i class="icofont icofont-simple-down"></i>
+                                </a>
                                 <!-- sub menu start -->
                                 @php($cars=\App\Models\Car::where('status','1')->get())
                                 <ul class="sub-menu left">
                                     @foreach($cars as $car)
-                                        <li><a href="{{route('carFrontView',$car->id)}}">{{$car->name_ar}}</a></li>
+                                        <li><a href="{{route('carFrontView',$car->id)}}">
+                                                {{ app()->getLocale() == 'en' ? $car->name_en : $car->name_ar }}
+                                            </a></li>
                                     @endforeach
                                 </ul>
                                 <!-- sub menu end -->
+                            </li>
+                            <li class="has-sub">
+                                <a href="javascript:void(this);">{{App\Helpers\Helper::translate('usizu_service')}}
+                                    <i class="icofont icofont-simple-down"></i>
+                                </a>
+                                <!-- sub menu start -->
+                                <ul class="sub-menu left">
+                                    @foreach($isuzu_services as $row)
+                                        <li><a href="{{route('serviceFrontView',$row->id)}}">
+                                                {{ app()->getLocale() == 'en' ? $row->name_en : $row->name_ar }}
+                                            </a></li>
+                                    @endforeach
+                                </ul>
+                                <!-- sub menu end -->
+                            </li>
+
+                            <li class="has-sub">
+                                <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Tenso')}}
+                                    <i class="icofont icofont-simple-down"></i>
+                                </a>
+                                <!-- sub menu start -->
+                                <ul class="sub-menu left">
+                                    @foreach($denso_services as $row)
+                                        <li><a href="{{route('serviceFrontView',$row->id)}}">
+                                                {{ app()->getLocale() == 'en' ? $row->name_en : $row->name_ar }}
+                                            </a></li>
+                                    @endforeach
+                                </ul>
+                                <!-- sub menu end -->
+                            </li>
+                            <li class="has-sub">
+                                <a href="javascript:void(this);">{{App\Helpers\Helper::translate('Services')}}
+                                    <i class="icofont icofont-simple-down"></i>
+                                </a>
+                                <!-- sub menu start -->
+                                <ul class="sub-menu left">
+                                    @foreach($services as $row)
+                                        <li><a href="{{route('serviceFrontView',$row->id)}}">
+                                                {{ app()->getLocale() == 'en' ? $row->name_en : $row->name_ar }}
+                                            </a></li>
+                                    @endforeach
+                                </ul>
+                                <!-- sub menu end -->
+                            </li>
+                            <li>
+                                <a href="{{route('front')}}#Partners">{{App\Helpers\Helper::translate('Partners')}}</a>
+                            </li>
+                            <li>
+                                <a href="{{route('front')}}#aboutCS">{{App\Helpers\Helper::translate('about company')}}</a>
                             </li>
                             <!-- single menu -->
 
                             <!-- single menu -->
 
                         </ul>
-
                     </nav>
                 </div>
             </div>
